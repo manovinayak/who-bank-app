7e1eed8a07665b0ae4f445b9cc9c30a8b53f0f09
Manovinayak Ayyappan <manovinayak@gmail.com>
2022-11-28 21:03:25 +1100

    feat(testimonial): added testimonial section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,13 +3,13 @@ import { feedback } from "../constants/index";
 import styles from "../styles";
 import FeedbackCard from "./FeedbackCard";
 
+// 1:50:45
 const Testimonials = () => (
   <section
     id={"clients"}
     className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
   >
-    {/* TODO*/}
-    <div />
+    <div className={"absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient"} />
     <div
       className={
         "w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]"
